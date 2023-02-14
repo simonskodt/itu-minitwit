7097d1104386e748a39c160a2d1a918c7927eca3
Vict2976 <vibr@itu.com>
2023-02-14 12:47:11 +0100

    feat: initial pages creation

--- MiniTwit/MiniTwit.Web/src/App.tsx
+++ MiniTwit/MiniTwit.Web/src/App.tsx
@@ -1,27 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Button } from '@mui/material';
-import { AppService } from './services/app.service';
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './index.css';
+import Login from "./pages/Login";
+import TimeLine from "./pages/TimeLine";
+import SignUp from "./pages/SignUp";
 
-function App() {
-
-  const appService = new AppService();
-
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Button
-          onClick={() => {
-            alert('clicked');
-            const response = appService.insertUser();
-          }}
-        >
-        Clich here to insert data
-    </Button>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<TimeLine />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </>
   );
 }
-
-export default App;
---
+++ MiniTwit/MiniTwit.Web/src/pages/SignUp.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Button } from '@mui/material';
+import { AppService } from '../services/app.service';
+
+function SignUp() {
+
+  const appService = new AppService();
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>THIS IS SIGNUP</div>
+        <Button
+          onClick={() => {
+            alert('clicked');
+            const response = appService.insertUser();
+          }}
+        >
+        Clich here to insert data
+    </Button>
+      </header>
+    </div>
+  );
+}
+
+export default SignUp;
