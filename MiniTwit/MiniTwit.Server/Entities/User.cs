63ac9c094a83aa211816eedd6acea780730a63a4
simonskodt <simon@skodt.dk>
2023-02-14 12:08:04 +0100

    chore(entities): initial version of user, follower and message

--- MiniTwit/MiniTwit.Server/Entities/User.cs
+++ MiniTwit/MiniTwit.Server/Entities/User.cs
@@ -1,10 +1,9 @@
-using MongoDB.Driver;
-using MongoDB.Bson; 
+using MongoDB.Bson;
 
 public class User
 {
-    public ObjectId _id { get; set;}
-    public string Username { get; set; }
-    public string Email { get; set; }
-    public string PwHash { get; set; }
+    public ObjectId _id { get; set; }
+    public string? Username { get; set; }
+    public string? Email { get; set; }
+    public string? PwHash { get; set; }
 }
