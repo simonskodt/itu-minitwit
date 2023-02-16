b3a6b777e2af8ec4e8195e987a80573fa19b4bee
Gustav Metnik-Beck <gustav.mb@outlook.com>
2023-02-16 17:38:36 +0100

    feat(MongoDBContext): Implemented mongodbcontext abstraction layer

--- MiniTwit.Core/IMongoDBContext.cs
+++ MiniTwit.Core/IMongoDBContext.cs
@@ -1,1 +1,12 @@
+using MiniTwit.Core.Entities;
+using MongoDB.Driver;
+
 namespace MiniTwit.Core;
+
+public interface IMongoDBContext
+{
+    IMongoDatabase MongoDatabase { get; init; }
+    IMongoCollection<User> Users { get; init; }
+    IMongoCollection<Follower> Followers { get; init; }
+    IMongoCollection<Message> Messages { get; init; }
+}
--- MiniTwit.Infrastructure/MongoDBContext.cs
+++ MiniTwit.Infrastructure/MongoDBContext.cs
@@ -1,1 +1,22 @@
+using MiniTwit.Core;
+using MiniTwit.Core.Entities;
+using MongoDB.Driver;
+
 namespace MiniTwit.Infrastructure;
+
+public class MongoDBContext : IMongoDBContext
+{
+    public IMongoDatabase MongoDatabase { get; init; }
+    public IMongoCollection<User> Users { get; init; }
+    public IMongoCollection<Follower> Followers { get; init; }
+
+    public IMongoCollection<Message> Messages { get; init; }
+
+    public MongoDBContext(string connectionString, string database)
+    {
+        MongoDatabase = new MongoClient("mongodb://localhost:27017").GetDatabase("minitwit");
+        Users = MongoDatabase.GetCollection<User>("user");
+        Followers = MongoDatabase.GetCollection<Follower>("follower");
+        Messages = MongoDatabase.GetCollection<Message>("message");
+    }
+}
