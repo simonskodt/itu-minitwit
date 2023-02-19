4725603d5cb63d4bc42341878b936218c0edaab0
Gustav Metnik-Beck <gustav.mb@outlook.com>
2023-02-19 11:45:05 +0100

    feat: MongoDB now loads configuration from secrets and container

--- MiniTwit.Core/IMongoDBContext.cs
+++ MiniTwit.Core/IMongoDBContext.cs
@@ -5,7 +5,6 @@ namespace MiniTwit.Core;
 
 public interface IMongoDBContext
 {
-    IMongoDatabase MongoDatabase { get; init; }
     IMongoCollection<User> Users { get; init; }
     IMongoCollection<Follower> Followers { get; init; }
     IMongoCollection<Message> Messages { get; init; }
--- run.sh
+++ run.sh
@@ -13,6 +13,15 @@ if [ "$#" -eq 0 ]; then
 elif [ "$1" = "stop" ]; then
     printf "${green}Stopping container\n${clear}"
     docker-compose stop
+elif [ "$1" = "secrets" ]; then
+    printf "${green}Creating secrets...\n${clear}"
+    project="MiniTwit.Server"
+    user="radiator"
+    password=$(<./.local/db_password.txt) # Load db password from file
+    connectionString="mongodb://$user:$password@localhost:27017"
+
+    dotnet user-secrets init --project $project
+    dotnet user-secrets set "ConnectionStrings:MiniTwit" "$connectionString" --project $project
 else
     printf "${red}Error: Unknown command\n${clear}"
 fi
