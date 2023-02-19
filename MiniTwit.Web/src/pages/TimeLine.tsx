d210a274509943301b919c06d0deb710f32ab050
simonskodt <simon@skodt.dk>
2023-02-19 18:03:53 +0100

    feat(timeline): add a quick timeline, need more work

--- MiniTwit.Web/src/pages/TimeLine.tsx
+++ MiniTwit.Web/src/pages/TimeLine.tsx
@@ -1,12 +1,28 @@
+import Footer from './Footer';
 import Layout from './Layout';
 import './Layout.css';
 
-function TimeLine() {
+interface Request {
+  endpoint: string;
+}
 
+
+// const TimelineTitle = ({r: Request, profile_user }) => {
+//   const endpoint = r.endpoint;
+//   if (endpoint === 'public_timeline') {
+//     return <h1>Public Timeline</h1>;
+//   } else if (endpoint === 'user_timeline') {
+//     return <h1>{profile_user.username}'s Timeline</h1>;
+//   }
+// };
+
+function TimeLine() {
   return (
     <div className="page">
-      <h1>Public Timeline</h1>
+      
       <Layout />
+        <h2>Public Timeline</h2>
+      <Footer />
     </div>
   );
 }
