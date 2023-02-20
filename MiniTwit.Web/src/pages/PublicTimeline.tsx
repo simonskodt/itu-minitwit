afc15f02c357d1963de1f3f963f7538719824b87
simonskodt <simon@skodt.dk>
2023-02-20 16:55:16 +0100

    feat(endpoint): add public page
    Co-authored-by: NikolajWLarsen <nikolajlarsen97@hotmail.com>

---
+++ MiniTwit.Web/src/pages/PublicTimeline.tsx
@@ -0,0 +1,22 @@
+import Footer from './Footer';
+import Layout from './Layout';
+import './Layout.css';
+import { fetchPublicTimeline } from './fetch';
+
+interface Request {
+  endpoint: string;
+}
+
+function TimeLine() {
+  return (
+    <div className="page">
+      
+      <Layout />
+        <h2>Public Timeline</h2>
+      <button onClick={() => fetchPublicTimeline()}>Fetch</button>
+      <Footer />
+    </div>
+  );
+}
+
+export default TimeLine
