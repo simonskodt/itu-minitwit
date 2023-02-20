2c7c5a225d3a58d66803368d82b30c60a2e4edae
simonskodt <simon@skodt.dk>
2023-02-20 16:56:24 +0100

    chore: delete

--- MiniTwit.Web/src/pages/TimeLine.tsx
+++ MiniTwit.Web/src/pages/TimeLine.tsx
@@ -6,16 +6,6 @@ interface Request {
   endpoint: string;
 }
 
-
-// const TimelineTitle = ({r: Request, profile_user }) => {
-//   const endpoint = r.endpoint;
-//   if (endpoint === 'public_timeline') {
-//     return <h1>Public Timeline</h1>;
-//   } else if (endpoint === 'user_timeline') {
-//     return <h1>{profile_user.username}'s Timeline</h1>;
-//   }
-// };
-
 function TimeLine() {
   return (
     <div className="page">
