0fedd5bb98d41f245c1c011ec18e86415d9507fe
Gustav Metnik-Beck <gustav.mb@outlook.com>
2023-02-26 21:29:09 +0100

    feat: Implemented integration test framework

--- MiniTwit.Server/Program.cs
+++ MiniTwit.Server/Program.cs
@@ -71,3 +71,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+public partial class Program {}
---
+++ MiniTwit.Tests/Server.Integration.Tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,86 @@
+using System.Globalization;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using MiniTwit.Core;
+using MiniTwit.Infrastructure;
+using MiniTwit.Infrastructure.Data.DataCreators;
+using Mongo2Go;
+
+namespace MiniTwit.Tests.Integration;
+
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override IHost CreateHost(IHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var mongoDBContext = services.SingleOrDefault(m => m.ServiceType == typeof(MongoDBContext));
+
+            if (mongoDBContext != null)
+            {
+                services.Remove(mongoDBContext);
+            }
+            
+            var runner = MongoDbRunner.Start(additionalMongodArguments: "--quiet --logpath /dev/null");
+            var settings = new MiniTwitDatabaseSettings
+            {
+                ConnectionString = runner.ConnectionString,
+                Database = "MiniTwit",
+                UsersCollectionName = "Users",
+                TweetsCollectionName = "Messages",
+                FollowersCollectionName = "Followers"
+            };
+
+            services.AddScoped<IMongoDBContext, MongoDBContext>(c => new MongoDBContext(Options.Create<MiniTwitDatabaseSettings>(settings)));
+
+            var provider = services.BuildServiceProvider();
+            using var scope = provider.CreateScope();
+            var appContext = scope.ServiceProvider.GetRequiredService<IMongoDBContext>();
+            Seed(appContext);
+        });
+
+        builder.UseEnvironment("Integration");
+        return base.CreateHost(builder);
+    }
+
+    private void Seed(IMongoDBContext context)
+    {
+        // Users
+        var gustav  = UserCreator.Create("000000000000000000000001", "Gustav", "g@minitwit.com", "password");
+        var simon   = UserCreator.Create("000000000000000000000002", "Simon", "s@minitwit.com", "password");
+        var nikolaj = UserCreator.Create("000000000000000000000003", "Nikolaj", "n@minitwit.com", "password");
+        var victor  = UserCreator.Create("000000000000000000000004", "Victor", "v@minitwit.com", "password");
+
+        context.Users.InsertMany(new[] { gustav, simon, nikolaj, victor });
+
+        // Messages
+        var m1 = MessageCreator.Create("000000000000000000000001", gustav.Id!, "Gustav's first tweet!", DateTime.Parse("01/01/2023 12:00:00", CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal));
+        var m2 = MessageCreator.Create("000000000000000000000002", gustav.Id!, "Gustav's second tweet!", DateTime.Parse("01/01/2023 12:00:00", CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal));
+        var m3 = MessageCreator.Create("000000000000000000000003", gustav.Id!, "Gustav's Flagged", DateTime.Parse("01/01/2023 12:00:01", CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal), 1);
+
+        var m4 = MessageCreator.Create("000000000000000000000004", simon.Id!, "Simon's first tweet", DateTime.Parse("01/01/2023 12:00:02", CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal));
+        var m5 = MessageCreator.Create("000000000000000000000005", simon.Id!, "Simon's second tweet", DateTime.Parse("01/01/2023 12:00:03", CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal));
+        var m6 = MessageCreator.Create("000000000000000000000006", simon.Id!, "Simon's third tweet", DateTime.Parse("01/01/2023 12:00:04", CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal));
+
+        var m7 = MessageCreator.Create("000000000000000000000007", nikolaj.Id!, "Nikolaj1", DateTime.Parse("01/01/2023 12:00:05", CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal));
+        var m8 = MessageCreator.Create("000000000000000000000008", nikolaj.Id!, "Nikolaj2", DateTime.Parse("01/01/2023 12:00:06", CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal));
+
+        var m9 = MessageCreator.Create("000000000000000000000009", victor.Id!, "Victor1", DateTime.Parse("01/01/2023 12:00:01", CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal));
+        var m10 = MessageCreator.Create("000000000000000000000010", victor.Id!, "Victor2", DateTime.Parse("01/01/2023 12:00:02", CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal));
+
+        context.Messages.InsertMany(new[] { m1, m2, m3, m4, m5, m6, m7, m8, m9, m10 });
+
+        // Followers
+        var f1 = FollowerCreator.Create(gustav.Id!, simon.Id!);
+
+        var f2 = FollowerCreator.Create(simon.Id!, nikolaj.Id!);
+        var f3 = FollowerCreator.Create(simon.Id!, victor.Id!);
+
+        var f4 = FollowerCreator.Create(victor.Id!, gustav.Id!);
+
+        context.Followers.InsertMany(new [] { f1, f2, f3, f4 });
+    }
+}
