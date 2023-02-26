30322fb4b7a8650043ac202c06fbe884953ae49c
Gustav Metnik-Beck <gustav.mb@outlook.com>
2023-02-26 17:40:16 +0100

    feat: Added annotations to fields

--- MiniTwit.Core/DTOs/LoginDTO.cs
+++ MiniTwit.Core/DTOs/LoginDTO.cs
@@ -1,5 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
 public record LoginDTO
 {
+    [Required]
     public string? Username { get; init; }
+    [Required]
     public string? Password { get; init; }
 }
--- MiniTwit.Core/DTOs/RegisterDTO.cs
+++ MiniTwit.Core/DTOs/RegisterDTO.cs
@@ -1,6 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 public record RegisterDTO
 {
+    [Required]
     public string? Username { get; init; }
+    [Required]
+    [EmailAddress]
     public string? Email { get; init; }
+    [Required]
     public string? Password { get; init; }
 }
