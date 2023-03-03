010e40012c2d133abb13713b78af5caecc67195d
Gustav Metnik-Beck <gustav.mb@outlook.com>
2023-03-03 11:43:36 +0100

    refactor: Refactored responses for Services/repositories

---
+++ MiniTwit.Core/Responses/HTTPResponse.cs
@@ -0,0 +1,12 @@
+namespace MiniTwit.Core.Responses;
+
+public enum HTTPResponse
+{
+    Ok = 200,
+    Created = 201,
+    NoContent = 204,
+    BadRequest = 400,
+    Unauthorized = 401,
+    NotFound = 404,
+    Conflict = 409
+}
---
+++ MiniTwit.Core/Responses/Response.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using MiniTwit.Core.Error;
+
+namespace MiniTwit.Core.Responses;
+
+public class Response
+{
+    public HTTPResponse HTTPResponse { get; }
+    public APIError? Error { get; }
+
+    public Response(HTTPResponse httpResponse, ErrorType? errorType = null)
+    {
+        HTTPResponse = httpResponse;
+        Error = errorType == null ? null : errorType.ToAPIError(HTTPResponse);
+    }
+
+    public ActionResult ToActionResult() => ToActionResult();
+
+    protected ActionResult ToActionResult(object? model = null, string location = "") => HTTPResponse switch
+    {
+        HTTPResponse.Ok => new OkObjectResult(model),
+        HTTPResponse.Created => new CreatedResult(location, model),
+        HTTPResponse.NoContent => new NoContentResult(),
+        HTTPResponse.BadRequest => new BadRequestObjectResult(Error),
+        HTTPResponse.NotFound => new NotFoundResult(),
+        HTTPResponse.Conflict => new ConflictObjectResult(Error),
+        _ => throw new NotSupportedException($"{HTTPResponse} not supported!")
+    };
+}
+
+public class Response<T> : Response
+{
+    public T? Model { get; }
+
+    public Response(HTTPResponse httpResponse, T? model, ErrorType? errorType = null) : base(httpResponse, errorType)
+    {
+        Model = model;
+    }
+
+    public ActionResult<T> ToActionResult(string location) => base.ToActionResult(Model, location);
+    public new ActionResult<T> ToActionResult() => base.ToActionResult(Model);
+}
