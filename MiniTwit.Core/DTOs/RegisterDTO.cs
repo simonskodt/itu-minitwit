057368c4dcf251a21ac3c622c83235b0eea968e2
Gustav Metnik-Beck <gustav.mb@outlook.com>
2023-03-03 11:44:39 +0100

    refactor: Refactored/merged existing DTOs and added new ones

--- MiniTwit.Core/DTOs/RegisterDTO.cs
+++
@@ -1,8 +0,0 @@
-namespace MiniTwit.Core.DTOs;
-
-public record RegisterDTO
-{
-    public string? Username { get; init; }
-    public string? Email { get; init; }
-    public string? Password { get; init; }
-}
