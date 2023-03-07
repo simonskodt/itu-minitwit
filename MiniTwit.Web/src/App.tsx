cd31dc48e99be0dbac2b21f5b844fd64bc5a9413
Gustav Metnik-Beck <gustav.mb@outlook.com>
2023-03-07 17:57:14 +0100

    chor: Renamed to Dev

--- MiniTwit.Web/src/App.tsx
+++ MiniTwit.Web/src/App.tsx
@@ -6,10 +6,10 @@ import TimeLine from "./pages/TimeLine";
 import PublicTimeline from "./pages/PublicTimeline";
 import SignUp from "./pages/SignUp";
 
-const LOCALHOST = "https://localhost:7111/";
+const DEVELOPMENT = "https://localhost:7111/";
 const PRODUCTION = "http://164.92.167.188:80/";
 
-export const API_URL = process.env.NODE_ENV === 'development' ? LOCALHOST : PRODUCTION;
+export const API_URL = process.env.NODE_ENV === 'development' ? DEVELOPMENT : PRODUCTION;
 
 export default function App() {
   return (
