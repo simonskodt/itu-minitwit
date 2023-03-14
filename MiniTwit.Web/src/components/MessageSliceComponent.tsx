af66b92bb3daf5904066f97b609cc5db7abaceb1
simonskodt <simon@skodt.dk>
2023-03-14 22:13:22 +0100

    fix: extra indentaion of div

--- MiniTwit.Web/src/components/MessageSliceComponent.tsx
+++ MiniTwit.Web/src/components/MessageSliceComponent.tsx
@@ -3,18 +3,18 @@ import Header from "./Header";
 import "../pages/Layout.css";
 import { FetchPublicTimeline } from "../pages/fetch";
 import { useState } from "react";
-import {MessageObjectWithName } from "../builders/interface";
+import { MessageObjectWithName } from "../builders/interface";
 import { useEffect } from "react";
 import { Message } from "../pages/Message";
 import { checkLogIn } from "../builders/functions";
 
 interface Props {
-    pageNumber: number;
+  pageNumber: number;
 }
 
 const MessageSliceComponent: React.FC<Props> = ({ pageNumber }) => {
 
-    const [AllMessages, setMessages] = useState<MessageObjectWithName[]>();
+  const [AllMessages, setMessages] = useState<MessageObjectWithName[]>();
 
   useEffect(() => {
     FetchPublicTimeline(pageNumber).then((messages) => {
@@ -22,32 +22,32 @@ const MessageSliceComponent: React.FC<Props> = ({ pageNumber }) => {
     });
   }, []);
 
-  if (AllMessages !=undefined ){
+  if (AllMessages != undefined) {
     return (
-        <div className="body">
-          {AllMessages?.map((mes) => (
-              <view key={mes.messageId}>
-                <view>
-                  <Message
-                    username={mes.userName}
-                    text={mes.text}
-                    date={mes.pubDate}
-                  />
-                </view>
-              </view>
-            ))}
-      </div>
+      <>
+        {AllMessages?.map((mes) => (
+          <view key={mes.messageId}>
+            <view>
+              <Message
+                username={mes.userName}
+                text={mes.text}
+                date={mes.pubDate}
+              />
+            </view>
+          </view>
+        ))}
+      </>
     );
-    } else {
+  } else {
     return (
-        <div>
-          <h2>Public TimeLine</h2>
-          <ul className="messages">
-            <li>
-              <p></p>
-            </li>
-          </ul>
-        </div>
+      <div>
+        <h2>Public TimeLine</h2>
+        <ul className="messages">
+          <li>
+            <p></p>
+          </li>
+        </ul>
+      </div>
     );
   }
 }
