17229c794d643f758c1b934d34890d588c92583f
simonskodt <simon@skodt.dk>
2023-03-14 18:12:02 +0100

    feat(hash): add md5 algo

--- MiniTwit.Web/src/pages/Message.tsx
+++ MiniTwit.Web/src/pages/Message.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import {Md5} from 'ts-md5';
 
 interface IMessageProps {
     username: string;
@@ -6,15 +6,22 @@ interface IMessageProps {
     date: string;
 }
 
+// Generation of MD5 hash to use as Gravatar image
+function generateHash(name: any): string {
+    var hash:string = Md5.hashStr(name);
+    console.log('https://www.gravatar.com/avatar/' + hash);
+    return 'https://www.gravatar.com/avatar/' + hash + '?s=50&d=identicon';
+}
+
 export function Message(props: IMessageProps) {
     return (
         <ul className='messages'>
             <li>
-                <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fitxdesign.com%2Fwhy-is-a-gravatar-something-you-should-start-using-immediately%2F&psig=AOvVaw0yw7lIV_6xksyBFbyzZMLn&ust=1677065371349000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCMDjncHBpv0CFQAAAAAdAAAAABAY"/>
+                <img src={generateHash(props.username)}/>
                 <p>
                     <a href={props.username}>{props.username}</a>&nbsp;
                     { props.text }&nbsp;
-                    <small>&mdash; &nbsp; { props.date }</small>
+                    <small>&mdash; { props.date }</small>
                 </p>
             </li>
         </ul>
