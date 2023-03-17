c5d594abb384bf6bf9adbdfd601c33e193d67262
simonskodt <simon@skodt.dk>
2023-03-17 13:02:09 +0100

    feat(date): format the datetime

--- MiniTwit.Web/src/pages/Message.tsx
+++ MiniTwit.Web/src/pages/Message.tsx
@@ -1,4 +1,4 @@
-import {Md5} from 'ts-md5';
+import { Md5 } from 'ts-md5';
 
 interface IMessageProps {
     username: string;
@@ -8,20 +8,24 @@ interface IMessageProps {
 
 // Generation of MD5 hash to use as Gravatar image
 function generateHash(name: string): string {
-    var hash:string = Md5.hashStr(name);
-    console.log('https://www.gravatar.com/avatar/' + hash);
+    var hash: string = Md5.hashStr(name);
     return 'https://www.gravatar.com/avatar/' + hash + '?s=50&d=identicon';
+}
+
+// Formatting of datetime, so it is more readable
+function formatDatetime(date: string): string {
+    return new Date(date).toLocaleString();
 }
 
 export function Message(props: IMessageProps) {
     return (
-        <ul className='messages'>
+        <ul className="messages">
             <li>
-                <img src={generateHash(props.username)}/>
+                <img src={generateHash(props.username)} />
                 <p>
                     <a href={props.username}>{props.username}</a>&nbsp;
-                    { props.text }&nbsp;
-                    <small>&mdash; { props.date }</small>
+                    {props.text}&nbsp;
+                    <small>&mdash; {formatDatetime(props.date)}</small>
                 </p>
             </li>
         </ul>
