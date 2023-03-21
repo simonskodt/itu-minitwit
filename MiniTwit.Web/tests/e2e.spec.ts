035ee8572583bdb05ded1df7fba1fc6933226187
niwl <niwl@itu.dk>
2023-03-21 13:54:42 +0100

    randomUsers, otherwise test fails

--- MiniTwit.Web/tests/e2e.spec.ts
+++
@@ -1,5 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('Register user via GUI and check db entry', async ({ page }) => {
-    
-});
--- MiniTwit.Web/tests/ui.spec.ts
+++ MiniTwit.Web/tests/ui.spec.ts
@@ -6,46 +6,25 @@ test('MiniTwit title', async ({ page }) => {
   await expect(page).toHaveTitle('MiniTwit');
 });
 
+
 test('Register user via GUI', async ({ page }) => {
   await page.goto('http://164.92.167.188:3000/register');
 
-  // Get all input elements on the page
+  //create randomUsername, because databse fails if not a unique username
+  const randomName = Math.random().toString(36).slice(2, 7);
   const inputElements = await page.$$('input');
 
   const userName = await inputElements[0].click();
-  await page.keyboard.type('samd');
+  await page.keyboard.type("UiTest"+randomName);
 
   const email = await inputElements[1].click();
-  await page.keyboard.type('samd@itu.dk');
+  await page.keyboard.type(randomName+'@itu.dk');
 
   const password = await inputElements[2].click();
-  await page.keyboard.type('samd');
+  await page.keyboard.type('123');
 
   const passwordRepeat = await inputElements[3].click();
-  await page.keyboard.type('samd');
-
-  await page.click('button:text("Sign Up")');
-
-  await expect(page).toHaveURL("http://164.92.167.188:3000/public");
-});
-
-test('test_register_user_via_gui_and_check_db_entry', async ({ page }) => {
-  await page.goto('http://164.92.167.188:3000/register');
-
-  // Get all input elements on the page
-  const inputElements = await page.$$('input');
-
-  const userName = await inputElements[0].click();
-  await page.keyboard.type('samd');
-
-  const email = await inputElements[1].click();
-  await page.keyboard.type('samd@itu.dk');
-
-  const password = await inputElements[2].click();
-  await page.keyboard.type('samd');
-
-  const passwordRepeat = await inputElements[3].click();
-  await page.keyboard.type('samd');
+  await page.keyboard.type('123');
 
   await page.click('button:text("Sign Up")');
 
