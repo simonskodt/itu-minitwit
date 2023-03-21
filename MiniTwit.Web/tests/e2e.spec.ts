b4a28c2fef62bbc8cbaa6e9ac368df1c67cf0707
niwl <niwl@itu.dk>
2023-03-21 12:04:38 +0100

    feat(testUI): made register UI test
    Co-authored-by: Simon Johann Skødt <simonskodt@users.noreply.github.com>

---
+++ MiniTwit.Web/tests/e2e.spec.ts
@@ -0,0 +1,5 @@
+import { test, expect } from '@playwright/test';
+
+test('Register user via GUI and check db entry', async ({ page }) => {
+    
+});
--- MiniTwit.Web/tests/ui.spec.ts
+++ MiniTwit.Web/tests/ui.spec.ts
@@ -6,12 +6,48 @@ test('MiniTwit title', async ({ page }) => {
   await expect(page).toHaveTitle('MiniTwit');
 });
 
-test('Register user via ', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('Register user via GUI', async ({ page }) => {
+  await page.goto('http://164.92.167.188:3000/register');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  // Get all input elements on the page
+  const inputElements = await page.$$('input');
 
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
+  const userName = await inputElements[0].click();
+  await page.keyboard.type('samd');
+
+  const email = await inputElements[1].click();
+  await page.keyboard.type('samd@itu.dk');
+
+  const password = await inputElements[2].click();
+  await page.keyboard.type('samd');
+
+  const passwordRepeat = await inputElements[3].click();
+  await page.keyboard.type('samd');
+
+  await page.click('button:text("Sign Up")');
+
+  await expect(page).toHaveURL("http://164.92.167.188:3000/public");
+});
+
+test('test_register_user_via_gui_and_check_db_entry', async ({ page }) => {
+  await page.goto('http://164.92.167.188:3000/register');
+
+  // Get all input elements on the page
+  const inputElements = await page.$$('input');
+
+  const userName = await inputElements[0].click();
+  await page.keyboard.type('samd');
+
+  const email = await inputElements[1].click();
+  await page.keyboard.type('samd@itu.dk');
+
+  const password = await inputElements[2].click();
+  await page.keyboard.type('samd');
+
+  const passwordRepeat = await inputElements[3].click();
+  await page.keyboard.type('samd');
+
+  await page.click('button:text("Sign Up")');
+
+  await expect(page).toHaveURL("http://164.92.167.188:3000/public");
 });
