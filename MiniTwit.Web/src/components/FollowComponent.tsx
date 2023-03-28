87d68d1e10d3093cf2ee02283f6320d541225048
simonskodt <simon@skodt.dk>
2023-03-28 15:19:51 +0200

    fix(follower): make only follow or unfollow show, not both

--- MiniTwit.Web/src/components/FollowComponent.tsx
+++ MiniTwit.Web/src/components/FollowComponent.tsx
@@ -2,7 +2,7 @@ import React, { Component, useState } from 'react';
 import './MessageComponent.css'
 import { API_URL } from '../App';
 import { AppService } from '../services/app.service'
-
+import { ButtonGroup } from '@mui/material';
 
 interface Props {
     isLoggedIn: boolean;
@@ -10,38 +10,56 @@ interface Props {
 }
 
 const FollowComponent: React.FC<Props> = ({ isLoggedIn, userToFollow }) => {
-
     const appService = new AppService();
-    const userName = sessionStorage.getItem('username')
+    const userName = sessionStorage.getItem('username');
 
-    function Follow(username: any){
-        appService.getUserId(username).then((result)=>{
-            const id = result.data.id
-            appService.Follow(userToFollow, id).then((fol) =>{
-                console.log(fol)
+    const [isFollowing, setIsFollowing] = useState(false);
+    const [followButtonText, setFollowButtonText] = useState('Follow');
+
+    function HandleFollow() {
+        appService.getUserId(String(userName)).then((res) => {
+            const id = res.data.id;
+            appService.getIsFollowing(id, userToFollow).then((isFollowing) => {
+                if (isFollowing.data) {
+                    UnFollow(userName);
+                    setFollowButtonText('Follow');
+                    setIsFollowing(false);
+                } else {
+                    Follow(userName);
+                    setFollowButtonText('Unfollow');
+                    setIsFollowing(true);
+                }
+            });
+        });
+    }
+
+    function Follow(username: any) {
+        appService.getUserId(username).then((result) => {
+            const id = result.data.id;
+            appService.Follow(userToFollow, id).then((fol) => {
+                alert('You are now following ' + userToFollow);
             })
         })
     }
 
-    function UnFollow(username: any){
-        appService.getUserId(username).then((result)=>{
-            const id = result.data.id
-            appService.UnFollow(userToFollow, id).then((fol) =>{
-                console.log(fol)
+    function UnFollow(username: any) {
+        appService.getUserId(username).then((result) => {
+            const id = result.data.id;
+            appService.UnFollow(userToFollow, id).then((fol) => {
+                alert('You are no longer following ' + userToFollow);
             })
         })
     }
 
-    if(isLoggedIn){
+    if (isLoggedIn) {
         return (
             <div>
-            <button onClick={()=> Follow(userName)}>FOLLOW</button>
-            <button onClick={()=>UnFollow(userName)}>UNFOLLOW</button>
+                <button onClick={() => HandleFollow()}>{followButtonText}</button>
             </div>
         );
-    }else{
-        return(
-        <div></div>
+    } else {
+        return (
+            <div></div>
         )
     }
 }
