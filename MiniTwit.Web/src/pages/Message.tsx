7231b8c8669b75e20da87a985ef88e1bdf90d54f
simonskodt <simon@skodt.dk>
2023-04-05 15:44:42 +0200

    feat: add new transparent image

--- MiniTwit.Web/src/pages/Message.tsx
+++ MiniTwit.Web/src/pages/Message.tsx
@@ -14,6 +14,12 @@ function generateHash(name: string): string {
     return 'https://www.gravatar.com/avatar/' + hash + '?s=50&d=identicon';
 }
 
+// Transparant background for identicon
+function generateHash2(name: string): string {
+    const hash: string = Md5.hashStr(name);
+    return 'https://api.dicebear.com/5.x/identicon/svg?seed=' + hash;
+}
+
 // Formatting of datetime, so it is more readable
 function formatDatetime(date: string): string {
     return new Date(date).toLocaleString();
@@ -23,9 +29,9 @@ export function Message(props: IMessageProps) {
     return (
         <ul className="messages">
             <li>
-                <img src={generateHash(props.username)} />
+                <img src={generateHash2(props.username)} />
                 <p>
-                    <a href={props.username}>{props.username}</a>&nbsp;
+                    <b><a href={props.username}>{props.username}</a></b>&nbsp;
                     {props.text}&nbsp;
                     <small>&mdash; {formatDatetime(props.date)}</small>
                 </p>
