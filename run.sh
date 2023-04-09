6778fdf743bc6a13b3b727b10aeb3b8b7867e7f2
Gustav Metnik-Beck <gustav.mb@outlook.com>
2023-04-09 19:48:16 +0200

    feat: Implemented Loki logging

--- run.sh
+++ run.sh
@@ -13,14 +13,13 @@ function start_backend {
     println "Starting mongodb container..." "$green"
     
     password=$(<./.local/db_password.txt) # Load db password
-    volume=""
     
     if $1 ; then
         docker volume create mongodb_data_container_local
-        volume="-v mongodb_data_container_local:/data/db"
+        docker run --rm --name mongodb -d -p 27018:27017 -v "mongodb_data_container_local:/data/db" -e "MONGO_INITDB_ROOT_USERNAME=radiator" -e "MONGO_INITDB_ROOT_PASSWORD=$password" -e "MONGO_INITDB_DATABASE=MiniTwit" mongo:latest
+    else
+        docker run --rm --name mongodb -d -p 27018:27017 -e "MONGO_INITDB_ROOT_USERNAME=radiator" -e "MONGO_INITDB_ROOT_PASSWORD=$password" -e "MONGO_INITDB_DATABASE=MiniTwit" mongo:latest
     fi
-    
-    docker run --rm --name mongodb -d -p 27018:27017 "$volume" -e "MONGO_INITDB_ROOT_USERNAME=radiator" -e "MONGO_INITDB_ROOT_PASSWORD=$password" -e "MONGO_INITDB_DATABASE=MiniTwit" mongo:latest
     
     println "Starting backend..." "$green"
     dotnet run --project MiniTwit.Server
@@ -39,41 +38,16 @@ function setup_dotnet_secrets {
     dotnet user-secrets set "ConnectionStrings:MiniTwit" "$connectionString" --project MiniTwit.Server
 }
 
-function setup_elk {
-    if [ -z "${ELK_DIR}" ]; then 
-        # Export env variables (set ELK_DIR to directory where this script is located)
-        ELK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-        ELK_USER="radiator"
-
-        export ELK_DIR 
-        export ELK_USER
-
-        echo "export ELK_DIR=$ELK_DIR" >> ~/.profile
-        echo "export ELK_USER=$ELK_USER" >> ~/.profile
-
-        # Go to ELK stack folder
-        (
-            cd "$ELK_DIR"/Logging || exit
-            # Change permissions on filebat config
-            sudo chown root filebeat.yml 
-            sudo chmod go-w filebeat.yml
-        )
-    else 
-        println "ELK_DIR already set; skipping..." "$red"
-    fi
-}
-
 function print_help {
-    println "--- RUN HELP ---"
+    println "--- RUN HELP ---" "$green"
     println "Available Commands:"
     println ">> Run the backend server and database with a mounted volume"
     println ">> nosave - Run the backend server and database with no mounted volume"
     println ">> secrets - Set up .NET user secrets for this project and saves a local developer connection string to the registry"
-    println ">> elk - Set up ELK environment"
     println ">> inspectdb - Use flag_tool to retrieve all messages from the database"
     println ">> flag <message_id>... - Use flag_tool to flag one or more messages given their ids."
     println ">> -h - Print this page"
-    println "----------------"
+    println "----------------" "$green"
 }
 
 if [ "$#" -eq 0 ]; then
@@ -82,8 +56,6 @@ elif [ "$1" = "nosave" ]; then
     start_backend false
 elif [ "$1" = "secrets" ]; then
     setup_dotnet_secrets
-elif [ "$1" = "elk" ]; then
-    setup_elk
 elif [ "$1" = "inspectdb" ]; then
     ./flag_tool -i | less
 elif [ "$1" = "flag" ]; then
