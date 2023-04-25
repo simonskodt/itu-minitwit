9882903f934c0ba08e5e9dce9f710b1c27a2265b
Gustav Metnik-Beck <gustav.mb@outlook.com>
2023-04-25 12:31:35 +0200

    feat: Setup swarm
    Co-authored-by: NikolajWLarsen <NikolajWLarsen@users.noreply.github.com>
    Co-authored-by: Simon Johann Skødt <simonskodt@users.noreply.github.com>

--- swarm.sh
+++ swarm.sh
@@ -5,6 +5,10 @@ red='\033[0;31m'
 green='\033[0;32m'
 clear='\033[0m'
 
+# Digital Ocean
+export BEARER_AUTH_TOKEN="Authorization: Bearer $DIGITAL_OCEAN_TOKEN"
+export JSON_CONTENT="Content-Type: application/json"
+
 function println {
     printf "%b%b\n%b" "$2" "$1" "$clear"
 }
@@ -23,11 +27,9 @@ function get_ip {
     done
 }
 
-function creatingDockerSwarmClusterNode {
+function createDockerSwarmClusterNode {
     export DIGITALOCEAN_PRIVATE_NETWORKING=true
     export DROPLETS_API="https://api.digitalocean.com/v2/droplets"
-    export BEARER_AUTH_TOKEN="Authorization: Bearer $DIGITAL_OCEAN_TOKEN"
-    export JSON_CONTENT="Content-Type: application/json"
 
     CONFIG='{"name":"Swarm-Manager","tags":["demo"],
         "size":"s-1vcpu-1gb", "image":"docker-20-04",
@@ -45,7 +47,7 @@ function creatingDockerSwarmClusterNode {
     println "Done." "$green"
 }
 
-function creatingWorkerNodes {
+function createWorkerNodes {
     println "Creating worker1 Droplet..." "$green"
     WORKER1_ID=$(curl -X POST $DROPLETS_API\
         -d'{"name":"worker1","tags":["demo"],"region":"fra1",
@@ -75,57 +77,92 @@ function creatingWorkerNodes {
     println "Done." "$green"
 }
 
-function makingSwarmManagerAClusterManager {
+function makeSwarmManagerClusterManager {
     println "Open the ports that Docker needs..." "$green"
-    ssh root@"$SWARM_MANAGER_IP" -i ~/.ssh/swarm "ufw allow 22/tcp && ufw allow 2376/tcp &&\
+    ssh root@"$SWARM_MANAGER_IP" -i ~/.ssh/devops "ufw allow 22/tcp && ufw allow 2376/tcp &&\
     ufw allow 2377/tcp && ufw allow 7946/tcp && ufw allow 7946/udp &&\
     ufw allow 4789/udp && ufw reload && ufw --force  enable &&\
     systemctl restart docker"
     println "Done." "$green"
 
     println "Initialize the swarm..." "$green"
-    ssh root@"$SWARM_MANAGER_IP" -i ~/.ssh/swarm "docker swarm init --advertise-addr $SWARM_MANAGER_IP"
-    ssh root@"$SWARM_MANAGER_IP" -i ~/.ssh/swarm "docker swarm join-token worker -q" SWMTKN-1-4rndqz4hwe38wtbl9fwgj33rk48ok3hri7a0xy42o7sf5ll38z-afkri2vu57m5z31v34bny16aj
-    WORKER_TOKEN=`ssh root@"$SWARM_MANAGER_IP" -i ~/.ssh/swarm "docker swarm join-token worker -q"`
+    ssh root@"$SWARM_MANAGER_IP" -i ~/.ssh/devops "docker swarm init --advertise-addr $SWARM_MANAGER_IP"
+    ssh root@"$SWARM_MANAGER_IP" -i ~/.ssh/devops "docker swarm join-token worker -q"
+    WORKER_TOKEN=`ssh root@"$SWARM_MANAGER_IP" -i ~/.ssh/devops "docker swarm join-token worker -q"`
     println "Done." "$green"
 
     println "Build a command that we can run on node-1 and node-2 to join the swarm...." "$green"
     REMOTE_JOIN_CMD="docker swarm join --token $WORKER_TOKEN $SWARM_MANAGER_IP:2377"
-    ssh root@"$WORKER1_IP" -i ~/.ssh/swarm "$REMOTE_JOIN_CMD"
-    ssh root@"$WORKER2_IP" -i ~/.ssh/swarm "$REMOTE_JOIN_CMD"
+    ssh root@"$WORKER1_IP" -i ~/.ssh/devops "$REMOTE_JOIN_CMD"
+    ssh root@"$WORKER2_IP" -i ~/.ssh/devops "$REMOTE_JOIN_CMD"
     println "Done." "$green"
 }
 
-function seeingStateOfClusterOnManager {
+function getClusterManagerState {
     println "Seeing the state of the cluster on the manager..." "$green"
-    ssh root@"$SWARM_MANAGER_IP" "docker node ls"
+    ssh root@"$SWARM_MANAGER_IP" -i ~/.ssh/devops "docker node ls"
     println "Done." "$green"
 }
 
-function startingService {
+function startService {
     println "Running a service on our cluster..." "$green"
-    ssh root@"$SWARM_MANAGER_IP" "docker service create -p 8080:8080 --name appserver stifstof/crashserver"
+    ssh root@"$SWARM_MANAGER_IP" -i ~/.ssh/devops "docker service create -p 8080:8080 --name appserver stifstof/crashserver"
     println "Done." "$green"
 }
 
-function checkingStateOfService {
+function checkService {
     println "Checking the state of the service..." "$green"
-    ssh root@$SWARM_MANAGER_IP "docker service ls"
+    ssh root@"$SWARM_MANAGER_IP" -i ~/.ssh/devops "docker service ls"
     println "Done." "$green"
 
     println "Checking the state of the service (appserver)..." "$green"
-    ssh root@$SWARM_MANAGER_IP "docker service ps appserver"
+    ssh root@"$SWARM_MANAGER_IP" -i ~/.ssh/devops "docker service ps appserver"
     println "Done." "$green"
 
     println "Open instance of manager ip in browser..." "$green"
-    open http://$SWARM_MANAGER_IP:8080
+    open http://"$SWARM_MANAGER_IP":8080
     println "Done." "$green"
 }
 
+function addScaling {
+    println "Adding scaling..." "$green"
+    ssh root@"$SWARM_MANAGER_IP" -i ~/.ssh/devops "docker service scale appserver=5"
+    ssh root@"$SWARM_MANAGER_IP" -i ~/.ssh/devops "docker service ls"
+    println "Done." "$green"
+
+    println "See current services running on the appserver..." "$green"
+    ssh root@"$SWARM_MANAGER_IP" -i ~/.ssh/devops "docker service ps appserver"
+    println "Done." "$green"
+}
+
+function routingMeshWorksAsAdvertised {
+    println "Open worker 1 browser..." "$green"
+    open http://"$WORKER1_IP":8080
+    println "Done." "$green"
+    
+    println "Open worker 2 browser..." "$green"
+    open http://"$WORKER2_IP":8080
+    println "Done." "$green"
+}
+
+function removeDroplets {
+    println "Removing swarm droplets..." "$red"
+    curl -X DELETE\
+      -H "$BEARER_AUTH_TOKEN" -H "$JSON_CONTENT"\
+      "https://api.digitalocean.com/v2/droplets?tag_name=demo"
+    println "Done." "$red"
+}
+
 if [ "$#" -eq 0 ]; then
-    creatingDockerSwarmClusterNode
-    creatingWorkerNodes
-    makingSwarmManagerAClusterManager
+    createDockerSwarmClusterNode
+    createWorkerNodes
+    makeSwarmManagerClusterManager
+    # getClusterManagerState
+    # startService
+    # checkService
+    # addScaling
+elif [ "$1" == "clean" ]; then
+    removeDroplets
 else
     println "Error: Unknown command" "$red"
     println "For a list of available commands type './run.sh -h'" "$red"
