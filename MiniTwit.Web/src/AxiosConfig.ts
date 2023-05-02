e599080299abd1270bf546da3135a6de52d75b8c
Gustav Metnik-Beck <gustav.mb@outlook.com>
2023-05-02 09:27:14 +0200

    chor: Reverted back to hardcoded dev url

--- MiniTwit.Web/src/AxiosConfig.ts
+++ MiniTwit.Web/src/AxiosConfig.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosRequestConfig } from "axios"
-
+// process.env.REACT_APP_DEV_BASE_URL
 const PRODUCTION_BASE_URL = "http://164.92.167.188:80/"
-const API_URL = process.env.NODE_ENV === 'development' ? process.env.REACT_APP_DEV_BASE_URL : PRODUCTION_BASE_URL
+const API_URL = process.env.NODE_ENV === 'development' ? "http://localhost:80" : PRODUCTION_BASE_URL
 
 export const requestConfig: AxiosRequestConfig = {
     maxBodyLength: Infinity,
